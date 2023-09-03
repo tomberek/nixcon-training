ada90e4584e43c00169877d8ea2b8613e93ac920
Tom Bereknyei <tomberek@gmail.com>
2023-09-03 12:28:47 -0400

    feat: api to fetch created_on when clicked

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import markupsafe
 import os
 
 import sqlite3
@@ -15,7 +16,10 @@ def hello_world():
     con = sqlite3.connect("local.db")
     cur = con.cursor()
     res = cur.execute("SELECT name FROM NAME").fetchall()
-    res = [f'<li>{x[0]}</li>' for x in res]
+    res = [f'''
+       <li hx-get="/api/{markupsafe.escape(x[0])}/created" hx-target="find span" hx-trigger="click throttle:1s">
+        {markupsafe.escape(x[0])}<span></span>
+       </li>''' for x in res]
     res = "\n".join(res)
     con.close()
     return f'''
@@ -24,6 +28,15 @@ def hello_world():
     {res}
     </ul>'''
 
+@app.route("/<name>/created",methods = ['GET'])
+def hello_name_created(name):
+    con = sqlite3.connect("local.db")
+    cur = con.cursor()
+    res = cur.execute(f'SELECT created_on FROM NAME WHERE name = ?',[name]).fetchall()
+    con.commit()
+    con.close()
+    return f':&nbsp;created on {markupsafe.escape("".join(res[0]))}'
+
 @app.route("/<name>",methods = ['POST','GET'])
 def hello_name(name):
     con = sqlite3.connect("local.db")
@@ -31,7 +44,7 @@ def hello_name(name):
     cur.execute('INSERT INTO name (name,created_on) VALUES(?,CURRENT_TIMESTAMP)',[name])
     con.commit()
     con.close()
-    return f"<p>Hello, {name}!</p>"
+    return f"<p>Hello, {markupsafe.escape(name)}!</p>"
 
 if __name__ == "__main__":
     app.run(port=(os.getenv("PORT",5000)),debug=True)
