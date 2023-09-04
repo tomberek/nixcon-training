7098f32472e841d505ff3c46121a244a2103921f
Tom Bereknyei <tomberek@gmail.com>
2023-09-03 21:06:55 -0400

    fix: small fixups, move create API endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import markupsafe
 import os
 
@@ -18,9 +18,10 @@ def hello_world():
     res = cur.execute("SELECT name FROM NAME").fetchall()
     res = [f'''
        <li hx-get="/api/{o}/created"
+           hx-disinherit="*"
            hx-target="find span"
-           hx-swap="innerHTML settle:1s"
-           hx-trigger="click throttle:2s">{o}<span></span>
+           hx-swap="outerHTML settle:1s"
+           hx-trigger="click throttle:1s">{o}<span></span>
        </li>''' for x in res if (o := markupsafe.escape(x[0]))]
     res = "\n".join(res)
     con.close()
@@ -35,12 +36,16 @@ def hello_name_created(name):
     con = sqlite3.connect("local.db")
     cur = con.cursor()
     res = cur.execute(f'SELECT created_on FROM NAME WHERE name = ?',[name]).fetchall()
+    stamp = markupsafe.escape("".join(res[0]))
     con.commit()
     con.close()
-    return f':&nbsp;created on {markupsafe.escape("".join(res[0]))}'
+    return f'''<span _="on click set my innerText to '' then halt">:&nbsp;created on {stamp}</span>'''
 
-@app.route("/<name>",methods = ['POST','GET'])
-def hello_name(name):
+@app.route("/",methods = ['POST'])
+def hello_name():
+    name=request.form.get('name','')
+    if name == "":
+        return f"unable to process '{name}'", 204
     con = sqlite3.connect("local.db")
     cur = con.cursor()
     cur.execute('INSERT INTO name (name,created_on) VALUES(?,CURRENT_TIMESTAMP)',[name])
